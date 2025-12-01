a677181974b2d1fdf4f17af88ae84aa9f5cee77d
xushiv12 <gyxu2024@gmail.com>
2025-11-30 20:34:29 -0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include"wlang.h" 
+A B C{
+	if(msg==WM_CREATE){
+		MessageBox(0,"CREATE WINDOW!","CREATE",MB_OK);
+		return 0;
+	}
+	return P1;
+}
+int API n{
+	W1=xushi;
+	W2=h;
+	W3="xu";
+	R;
+	HWND hwnd=CreateWindow(
+	"xu",
+	"A window",
+	WS_IT,
+	1,1,3000,3000,
+	0,0,h,0
+	);
+	ShowWindow(hwnd,bbb);
+	MSG msg;
+	run(msg);
+	return 0;
+}
